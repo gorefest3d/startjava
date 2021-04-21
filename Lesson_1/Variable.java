8c1b9ded9ca9a9c1bb7c3442de09feb32e77b774
Alex Sorokin <gorefest3d@mail.ru>
2021-04-22 00:46:15 +0300

    add Variable class

---
+++ Lesson_1/Variable.java
@@ -0,0 +1,21 @@
+public class Variable {
+    public static void main(String[] args) {
+        byte memory = 64;
+        short diskSpace = 30000;
+        int diskTwoSpace = 100000;
+        long usbDiskSpace = 125546987L;
+        float processorGhz = 3.30F;
+        double windowsCore = 19042.928D;
+        char processorGen = 'i';
+        boolean isWindows = true;
+
+        System.out.println(memory);
+        System.out.println(diskSpace);
+        System.out.println(diskTwoSpace);
+        System.out.println(usbDiskSpace);
+        System.out.println(processorGhz);
+        System.out.println(windowsCore);
+        System.out.println(processorGen);
+        System.out.println(isWindows);
+    }
+}
