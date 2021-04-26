f464871b7f2db71091c7ba7343319eb5fafe6447
Alex Sorokin <gorefest3d@mail.ru>
2021-04-27 00:07:25 +0300

    uppgate HW_1

--- Lesson_1/CalculatorMain.java
+++ Lesson_1/CalculatorMain.java
@@ -1,10 +1,10 @@
 public class CalculatorMain {
     public static void main(String[] args) {
-    int numberOne = 10;
-    int numberTwo = 3;
-    char operation = '/';
+        int numberOne = 10;
+        int numberTwo = 3;
+        char operation = '/';
 
-    if (operation == '+') {
+        if (operation == '+') {
             System.out.println(numberOne + numberTwo);
         } else if (operation == '-') {
             System.out.println(numberOne - numberTwo);
@@ -14,9 +14,9 @@ public class CalculatorMain {
             System.out.println((numberOne * 1.0) / numberTwo);
         } else if (operation == '^') {
             int result = 1;
-                for (int i = 1; i <= numberTwo; i++) {
-                    result *= numberOne;
-                }
+            for (int i = 1; i <= numberTwo; i++) {
+                result *= numberOne;
+            }
             System.out.println(result);
         } else if (operation == '%') {
             System.out.println(numberOne % numberTwo);
