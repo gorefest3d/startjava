53e7b98a610c5e6aaafadb81cae24d1535c233dc
Alex Sorokin <gorefest3d@mail.ru>
2021-05-13 04:21:18 +0300

    modified thiskj HW_1

--- src/com/startjava/lesson_2_3/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3/calculator/CalculatorTest.java
@@ -21,6 +21,8 @@ public class CalculatorTest {
             int numberTwo = scan.nextInt();
             calculator.setNumberTwo(numberTwo);
 
+
+
             calculator.calculate();
             scan.nextLine();
 
