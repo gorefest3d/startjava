64398def6ff7880273d751c3c28c14c5c1288009
Alex Sorokin <gorefest3d@mail.ru>
2021-05-22 15:12:21 +0300

    add modification to Calculator

--- src/com/startjava/lesson_2_3/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3/calculator/Calculator.java
@@ -9,13 +9,7 @@ public class Calculator {
             case "-" -> System.out.println(Integer.parseInt(words[0]) - Integer.parseInt(words[2]));
             case "*" -> System.out.println(Integer.parseInt(words[0]) * Integer.parseInt(words[2]));
             case "/" -> System.out.println((Integer.parseInt(words[0]) * 1.0) / Integer.parseInt(words[2]));
-            case "^" -> {
-                int result = 1;
-                for (int i = 1; i <= Integer.parseInt(words[2]); i++) {
-                    result *= Integer.parseInt(words[0]);
-                }
-                System.out.println(result);
-            }
+            case "^" -> System.out.println(Math.pow(Integer.parseInt(words[0]), Integer.parseInt(words[2])));
             case "%" -> System.out.println(Integer.parseInt(words[0]) % Integer.parseInt(words[2]));
         }
     }
